b064d0efedf78c75d09ae1d1fecc3ea89fa097cd
Henrique <rick.apocalypse@hotmail.com>
2023-04-15 14:41:13 -0300

    15/04_histogram_qiskit

---
+++ histogram_probabilities.py
@@ -0,0 +1,5 @@
+from qiskit import QuantumCircuit, BasicAer, execute
+from qiskit.visualization import plot_histogram
+from qiskit_ibm_provider import IBMProvider
+
+# The GHZ circuit is a quantum circuit where the qbits
