273b504b59fe8579ea838aa0e789a84915b2b437
Henrique <rick.apocalypse@hotmail.com>
2023-04-18 18:18:40 -0300

    update in the rand_key_quantum_state

--- rand_key_quantum_state.py
+++ rand_key_quantum_state.py
@@ -1,22 +1,26 @@
 import numpy as np
-from qiskit import QuantumCircuit, BasicAer, execute
+from qiskit import QuantumCircuit, BasicAer, execute, Aer
+from qiskit.visualization import plot_bloch_multivector
+import matplotlib.pyplot as plt
 
-qc = QuantumCircuit(1)
-if np.random.rand() >= 0.5:
-    qc.h(0)
 
-else:
+qc = QuantumCircuit(1)                              # build a circuit with the one qubit
+if np.random.rand() >= 0.5:                         # if the random number is greater than 0.5, append the hadamard gate in the circuit
+    qc.h(0)                                         # build a |+> 
+
+else:                                               # if the random number is not greater than 0.5
     qc.x(0)
-    qc.h(0)
+    qc.h(0)                                         # build a |-> 
 
-back_sv = BasicAer.get_backend('statevector_simulator')
-result = execute(qc, back_sv).result()
-qc_sv = result.get_statevector(qc)
-print(qc_sv)
+back_sv = Aer.get_backend('statevector_simulator')  # backend simulator for generator the statevector
+result_sv = execute(qc, back_sv).result()           # to execute the statevector of circuit "qc"
+qc_sv = result_sv.get_statevector(qc)               # stores the state vector
+plot_bloch_multivector(qc_sv)                       # plot bloch sphere for visualization of the random state
+plt.show()                                          # prints the state vector
 
 # verify the key quantum state
 
 if qc_sv[1] > 0:
-    print(f'|+> = {qc_sv}')
+    print(f'|+> = [{float(qc_sv[0])}, {float(qc_sv[1])}]')
 else:
-    print(f'|-> = {qc_sv}')
+    print(f'|-> = [{float(qc_sv[0])}, {float(qc_sv[1])}]')
